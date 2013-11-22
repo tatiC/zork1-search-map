bbdf248d72f1fffbf6331550a6c7fc68284fcc66
Tati Carvalho <tatigmc@yahoo.com.br>
2013-11-22 17:16:17 -0200

    Added directions and tested finding place and direction

--- zork.py
+++ zork.py
@@ -2,41 +2,55 @@
 
 # Testing
 
+''' STUFF
+Print all possible paths:
+for i in clearing:
+	print i, clearing[i]
+'''	
+
 import os
 
 os.system('clear')
 
-places = ["clearing", "kitchen"]
+places = ["clearing", "north_of_house", "south_of_house", "west_of_house"]
 
 def check_place(place):
 	return places.__contains__(place)
 	
-# clearing = {
-# 	"n": "forest(impenetrable)",
-# 	"s": "forest(dimly lit, large trees all aroun)",
-# 	"e": "canyon view",
-# 	"w": "forest(tree all directions)"
-# }
- 
-print "*" * 45
-print "Welcome to Zork Search Map"
-print "*" * 45
+def find_passage(place):
+	for key, values in zork_map.iteritems():
+	    if place in values:
+	        print key
+	        print directions[zork_map[key].index(place)]	
+	
+directions = {
+0: "n",
+1: "ne",
+2: "e",
+3: "se",
+4: "s",
+5: "sw",
+6: "w",
+7: "nw",
+}
 
-place_at = raw_input("Where are you? > ")
-while not check_place(place_at):
-	print "Don't know this place, try again!"
-	place_at = raw_input("Where are you? > ")
+zork_map = {
+	'west of house': ['north of house', 'x', 'door is boarded', 'x', 'south of house', 'x', 'forest 1', 'x'],
+    'south of house': ['windows all boarded', 'x', 'behind house', 'x', 'forest 2', 'x', 'west of house', 'x']
+    }
 
-destination = raw_input("Where do you want to go? > ") 
-while not check_place(destination):	
-	print "Don't know this place, try again!"
-	destination = raw_input("Where do you want to go? > ") 
+find_passage("forest 1")
 
-
-# if 
-# 	print "*" * 45
-# 	print "You're at %s and want to go to %s" % (place_at, destination)
-# 	print "*" * 45
-# else:
-# 	print "Ops!!! got an error!"
+# print "*" * 45
+# print "Welcome to Zork Search Map"
+# print "*" * 45
 # 
+# place_at = raw_input("Where are you? > ")
+# while not check_place(place_at):
+# 	print "Don't know this place, try again!"
+# 	place_at = raw_input("Where are you? > ")
+# 
+# destination = raw_input("Where do you want to go? > ") 
+# while not check_place(destination):	
+# 	print "Don't know this place, try again!"
+# 	destination = raw_input("Where do you want to go? > ") 
